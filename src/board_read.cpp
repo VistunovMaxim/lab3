78ce4c5a52fe1b8569d0b19215ef2838e6a3ffb4
Maxim Vistunov <max@MacBook-Pro-landlord.local>
2018-05-28 02:52:20 +0700

    Implemented chess movements

---
+++ src/board_read.cpp
@@ -0,0 +1,31 @@
+#include guard
+#include "board_read.hpp"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *board_read() {
+    char* str = (char*)calloc(7, sizeof(char));
+    int flag;
+    do {
+        scanf("%6s", str);
+        flag = (check_input(str));
+        if (flag) {
+            printf("Incorrect input, try again\n");
+        }
+    } while (flag);
+    return str;
+}
+
+int check_input(char *str) {
+    if (str[0] < 'a' || str[0] > 'h'
+        || str[1] < '1' || str[1] > '8'
+        || str[2] != '-'
+        || str[3] < 'a' || str[3] > 'h'
+        || str[4] < '1' || str[4] > '8'
+        || str[5] != '\0') {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+
+#include guard
+#include "board_start.hpp"
+#include "board_print_plain.hpp"
+#include "board.hpp"
+int main() {
+    char deck[8][8];
+    board_start(deck);
+    while(1) {
+        board_print_plain(deck);
+        board(deck);
+    }
+}
